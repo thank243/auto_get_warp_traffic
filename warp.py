5a06e3f1b8a06f323827253636bc89f405d60d94
Senis <thank243@gmail.com>
2021-11-07 13:44:14 +0800

    fix minor bugs.

--- warp.py
+++ warp.py
@@ -1,75 +1,74 @@
-import urllib.request
-import json
 import datetime
+import json
+import os
 import random
 import string
 import time
-import os
-import sys
-os.system('cls' if os.name == 'nt' else 'clear')
-print('Getting WARP+ Traffic on Github Actions')
-referrer = os.environ["DEVICEID"]
-def genString(stringLength):
-	try:
-		letters = string.ascii_letters + string.digits
-		return ''.join(random.choice(letters) for i in range(stringLength))
-	except Exception as error:
-		print(error)		    
-def digitString(stringLength):
-	try:
-		digit = string.digits
-		return ''.join((random.choice(digit) for i in range(stringLength)))    
-	except Exception as error:
-		print(error)	
-url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
-def run():
-	try:
-		install_id = genString(22)
-		body = {"key": "{}=".format(genString(43)),
-				"install_id": install_id,
-				"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
-				"referrer": referrer,
-				"warp_enabled": False,
-				"tos": datetime.datetime.now().isoformat()[:-3] + "+02:00",
-				"type": "Android",
-				"locale": "es_ES"}
-		data = json.dumps(body).encode('utf8')
-		headers = {'Content-Type': 'application/json; charset=UTF-8',
-					'Host': 'api.cloudflareclient.com',
-					'Connection': 'Keep-Alive',
-					'Accept-Encoding': 'gzip',
-					'User-Agent': 'okhttp/3.12.1'
-					}
-		req         = urllib.request.Request(url, data, headers)
-		response    = urllib.request.urlopen(req)
-		status_code = response.getcode()	
-		return status_code
-	except Exception as error:
-		print(error)	
+import urllib.request
 
-g = 0
-b = 0
+
+def digitString(string_len):
+    try:
+        digit = string.digits
+        return ''.join((random.choice(digit) for _ in range(string_len)))
+    except Exception as _:
+        print(_)
+
+
+def run():
+    def genString(string_len):
+        try:
+            letters = string.ascii_letters + string.digits
+            return ''.join(random.choice(letters) for _ in range(string_len))
+        except Exception as _:
+            print(_)
+
+    try:
+        install_id = genString(22)
+        body = {"key": "{}=".format(genString(43)),
+                "install_id": install_id,
+                "fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
+                "referrer": referrer,
+                "warp_enabled": False,
+                "tos": datetime.datetime.now().isoformat()[:-3] + "+02:00",
+                "type": "Android",
+                "locale": "es_ES"}
+        data = json.dumps(body).encode('utf8')
+        headers = {'Content-Type': 'application/json; charset=UTF-8',
+                   'Host': 'api.cloudflareclient.com',
+                   'Connection': 'Keep-Alive',
+                   'Accept-Encoding': 'gzip',
+                   'User-Agent': 'okhttp/3.12.1'
+                   }
+        req = urllib.request.Request(url, data, headers)
+        response = urllib.request.urlopen(req)
+        status_code = response.getcode()
+        return status_code
+    except Exception as error:
+        return error
+
+
+print('Getting WARP+ Traffic on Github Actions\n')
+referrer = os.environ["DEVICEID"]
+url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
+good = 0
+bad = 0
+
 while True:
-	result = run()
-	if result == 200:
-		g = g + 1
-		os.system('cls' if os.name == 'nt' else 'clear')
-		print("")
-		print("Getting WARP+ Traffic")
-		print(f"\n[-] WORK ON ID: {referrer}")    
-		print(f"[:)] {g} GB has been successfully added to your account.")
-		print(f"[#] Total: {g} Good {b} Bad")
-		if g == 50:
-		    print("Job compeleted!")
-		    break
-		print("[*] After 18 seconds, a new request will be sent.")
-		time.sleep(18)
-	else:
-		b = b + 1
-		os.system('cls' if os.name == 'nt' else 'clear')
-		print("")
-		print("Getting WARP+ Traffic")
-		print("\n[:(] Error when connecting to server.")
-		print(f"[#] Total: {g} Good {b} Bad")	
-		print("[*] After 5 seconds, a new request will be sent.")
-		time.sleep(5)
+    ret = run()
+    if ret == 200:
+        good = good + 1
+        print(f"[-] WORK ON ID: {referrer}\n"
+              f"[:)] {good} GB has been successfully added to your account.\n"
+              f"[#] Total: {good} Good {bad} Bad")
+        if good == 50:
+            print("Job completed!")
+            break
+        print("[*] After 18 seconds, a new request will be sent.\n")
+        time.sleep(18)
+    else:
+        bad = bad + 1
+        print(f"[:)] {ret}\n"
+              f"[#] Total: {good} Good {bad} Bad\n"
+              "[*] After 5 seconds, a new request will be sent.\n")
+        time.sleep(5)
